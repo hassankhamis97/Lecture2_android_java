b815fc4ac9438abc4b7d221c70272ac78136ec4c
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2021-08-21 23:14:27 +0200

    handle view binding

--- app/src/main/java/com/hk/lecture2_view_binding_view_model/MainActivity.java
+++ app/src/main/java/com/hk/lecture2_view_binding_view_model/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
+        binding.textView.setText("Welcome");
     }
 }
