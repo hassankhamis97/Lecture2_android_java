69017e5629708b00d71b777ada8ba37a5b820be4
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2021-08-21 23:53:18 +0200

    handle start view model

--- app/src/main/java/com/hk/lecture2_view_binding_view_model/MainActivity.java
+++ app/src/main/java/com/hk/lecture2_view_binding_view_model/MainActivity.java
@@ -2,8 +2,8 @@ package com.hk.lecture2_view_binding_view_model;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
-import android.database.DatabaseUtils;
 import android.os.Bundle;
 
 import com.hk.lecture2_view_binding_view_model.databinding.ActivityMainBinding;
@@ -11,10 +11,12 @@ import com.hk.lecture2_view_binding_view_model.model.User;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private MainViewModel mainViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         User user = new User("Hassan", "hassan@gmail.com", 25);
---
+++ app/src/main/java/com/hk/lecture2_view_binding_view_model/MainViewModel.java
@@ -0,0 +1,12 @@
+package com.hk.lecture2_view_binding_view_model;
+
+import android.util.Log;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    public MainViewModel() {
+        super();
+        Log.d("Lecture2", "MainViewModel: Start View model");
+    }
+}
